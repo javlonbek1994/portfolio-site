1c767bf953845d1ceac13d79018632f2f71d6947
javlonbek1994 <egamberdiyevbaxriddin65@gmail.com>
2026-05-25 11:09:36 +0500

    fix flask

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-app.run(debug=True)
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+    
